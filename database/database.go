e58195bcb9c65b2ef7435ea691eba65be6635893
Riker <riker.quintana@strategycomplex.dev>
2023-10-23 10:27:19 -0700

    init

---
+++ database/database.go
@@ -0,0 +1,68 @@
+package database
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+var username_, password_, dbName_ string
+var credentials_ Credentials
+
+type Credentials struct {
+	Databases []Database `json:"Database"`
+}
+
+type Database struct {
+	Name     string `json:"name"`
+	Type     string `json:"mysql"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func GetCredentials(jsonFile string) bool {
+	file, err := os.Open(jsonFile)
+	if err != nil {
+		fmt.Println("Error opening file, check if path is valid: ", err)
+		return false
+	}
+	defer file.Close()
+
+	// read our opened jsonFile as a byte array.
+	byteValue, _ := io.ReadAll(file)
+
+	err = json.Unmarshal(byteValue, credentials_)
+	if err != nil {
+		fmt.Println("Failed to unmarshal Credential type data, check json file: ", err)
+		return false
+	}
+
+	return true
+}
+
+func Connect(database string) {
+	var dt Database
+	for _, db := range credentials_.Databases {
+		if db.Name == database {
+			dt = db
+			break
+		}
+	}
+
+	d, err := gorm.Open(dt.Type, fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", dt.Name, dt.Password, dt.Name))
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
